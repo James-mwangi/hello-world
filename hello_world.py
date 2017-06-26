5489a7d7a6c0786a5e6fd8e72f524b92c11acc77
James-mwangi <jamesnderitumwangi@gmail.com>
2017-06-27 00:35:12 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,24 @@
-print('hello world')
+import turtle
+
+shape = input('Shape to be drawn.. ')
+side = input('Length of sides.. ')
+  
+  side = int(side)
+  
+if shape == 'triangle':
+  turtle.color(blue)
+  turtle.length(side)
+  turtle.left(360/3)
+  
+elif shape == 'circle':
+  turtle.color(green)
+  turtle.left(120, 180)
+  
+elif shape == 'tetrahedron':
+  for steps in range(3):
+    turtle.color(blue)
+    turtle.length(side)
+    turtle.left(360 / 3)
+  for steps in range(3):
+    turtle.length(blue)
+    turtle.left(360 / 3)
